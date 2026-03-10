12669b468acc09bcd811ace64c7eb246d47156cc
Alma Tamir <alma.tamir@post.runi.ac.il>
2026-03-10 14:46:15 +0200

    use environment variable for API URL

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,7 +13,8 @@ export default function Home() {
     setResult('');
     setGuess('');
     setIsCorrect(null);
-    const res = await fetch('http://127.0.0.1:5000/api/country?new=true');
+    //const res = await fetch('http://127.0.0.1:5000/api/country?new=true');
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/country`);
     const data = await res.json();
     setClues(data.clues);
     setCountryId(data.id);
@@ -24,7 +25,8 @@ export default function Home() {
   }, []);
 
   const submitGuess = async () => {
-    const res = await fetch('http://127.0.0.1:5000/api/guess', {
+    //const res = await fetch('http://127.0.0.1:5000/api/guess', {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/guess`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ guess, id: countryId }),
